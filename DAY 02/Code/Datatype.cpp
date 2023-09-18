5cc5cb80cdfb3af81c6e9be75fd489624f78963b
121Akash <akashdixit377@gmail.com>
2023-09-18 06:12:09 +0530

    DAY 02

---
+++ DAY 02/Code/Datatype.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std ;
+int main(){
+      
+      int a= 123;
+
+      cout<< a << endl;
+
+      char b= 'v';
+
+      cout<< b << endl;
+
+      bool bl = true;
+
+      cout<< bl << endl;
+
+
+      float f  =1.3;
+
+      cout<< f << endl;
+
+      double d = 1.234;
+
+      cout<< d<< endl;
+
+
+      // sizeof
+
+      int size  = sizeof(a);
+
+      cout<<"size of a :"<<size<<endl;
+
+       int size1  = sizeof(d);
+
+      cout<<"size of float :"<<size1<<endl;
+
+
+
+
+    int a = 'a';
+      cout<<a<<endl;
+
+     char ch= 98;
+
+     cout<<ch<<endl;
+
+
+
+
+
+
+
+
+
+
+
+}
